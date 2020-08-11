46e89b51fcb74744015e1a1d66e0643c8f2dab08
Nick Root <crecentnight@gmail.com>
2020-08-11 11:42:55 -0600

    first commit4

--- script.js
+++ script.js
@@ -891,7 +891,7 @@ function renderPlaces(places) {
    // model.setAttribute('rotation', '180');
 
 
-     model.setAttribute('scale', '1 1 1');
+     model.setAttribute('scale', '1.2 1.2 1.2');
     // model.setAttribute('color', 'green');
     // model.setAttribute('opacity', '0.75');
 
@@ -957,7 +957,7 @@ function staticLoadPlacesCU() {
         let modelCU = document.createElement('a-entity');
       modelCU.setAttribute('gps-entity-place', `latitude: ${latitudeCU}; longitude: ${longitudeCU};`);
       modelCU.setAttribute('gltf-model', 'models/CU-Logo-7-23-2020.gltf');
-      modelCU.setAttribute('scale', '6 6 6');
+      modelCU.setAttribute('scale', '8 8 8');
            //allows to rotate
       modelCU.setAttribute('animation', 'property: rotation; to: 0 360 0; loop:true; dur: 6000; easing: linear');
       modelCU.addEventListener('loaded', () => {
